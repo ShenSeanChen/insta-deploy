e51e3420817d21fede34c502fa0118218458368c
ShenSeanChen <seanchen9832@gmail.com>
2022-09-16 11:23:19 -0400

    small tweaks

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4d2d5b0a81f3eaca"; }
+/******/ 		__webpack_require__.h = function() { return "03a394a27f83d18e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
